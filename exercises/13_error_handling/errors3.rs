1a827f338ebbe66e81a282a2e5e435edb7446ff0
Baylor <airor@bmal.net>
2024-01-18 20:29:22 -0500

    Checkpoint2: reached traits

--- exercises/13_error_handling/errors3.rs
+++ exercises/13_error_handling/errors3.rs
@@ -7,20 +7,25 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input);
+    let true_cost = match cost {
+        Ok(cost) => cost,
+        Err(e) => {
+            eprintln!("Invalid quantity, please enter an integer");
+            return;
+        }
+    };
 
-    if cost > tokens {
+    if true_cost > tokens {
         println!("You can't afford that many!");
     } else {
-        tokens -= cost;
+        tokens -= true_cost;
         println!("You now have {} tokens.", tokens);
     }
 }
